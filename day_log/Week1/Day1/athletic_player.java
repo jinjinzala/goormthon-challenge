52fd9d88def1b70a30a21b57fe87cbb419ffb8c4
찐찐 <jinjinzala@naver.com>
2023-08-14 10:27:18 +0000

    주석 추가

--- day_log/Week1/Day1/athletic_player.java
+++ day_log/Week1/Day1/athletic_player.java
@@ -4,8 +4,8 @@ class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
-        String a[] = input.split(" "); 
-        double b = Integer.parseInt(a[0]) * (1 + (Integer.parseInt(a[1]) / 30.0)); 
+        String a[] = input.split(" "); //공백으로 들어오는 문자열을 잘라서 넣어주기
+        double b = Integer.parseInt(a[0]) * (1 + (Integer.parseInt(a[1]) / 30.0)); //숫자로 형변환하여 넣어주기 
         int result = (int) Math.floor(b); 
         System.out.print(result);
     }
